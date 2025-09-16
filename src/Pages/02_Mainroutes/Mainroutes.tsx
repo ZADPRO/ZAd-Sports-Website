f33458de689c4df173396d263e2cf25bd97095dd
yMahalakshmi <Mounika.t@zadroit.com>
2025-09-16 15:53:33 +0530

    url corrections

--- src/Pages/02_Mainroutes/Mainroutes.tsx
+++ src/Pages/02_Mainroutes/Mainroutes.tsx
@@ -2,7 +2,6 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Navigate,
 } from "react-router-dom";
 
 import Header from "../00_Header/Header";
@@ -14,12 +13,11 @@ import Login from "../05-Login/Login";
 import Dashboard from "../06-Dashboard/Dashboard";
 import Subscription from "../07-Subscription/Subscription";
 import TestimonialsPage from "../../Components/Testinomials/testimonials";
-// import Terms from "../03_Terms/Terms";
-// import Privacy from "../04_Privacy/Privacy";
 import Blogs from "../../Components/Blogs/blogs";
 import ScrollToTop from "../../Components/Blogs/BlogData";
 import ReleaseView from "../../Components/Release/ReleaseView";
 import BlogView from "../../Components/ListBlogs/BlogsView";
+
 function App() {
   return (
     <div>
@@ -27,9 +25,13 @@ function App() {
         <Header />
         <ScrollToTop />
         <Routes>
-         <Route path="/" element={<Home />} />
-          <Route path="/blogs" element={<Blogs />} />
+          {/* ✅ Keep `/` as Home */}
+          <Route path="/" element={<Home />} />
+
+          {/* ✅ Optional alias: `/home` also loads Home */}
           <Route path="/home" element={<Home />} />
+
+          <Route path="/blogs" element={<Blogs />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/subscription" element={<Subscription />} />
           <Route path="/privacy-policy" element={<Privacy />} />
@@ -37,8 +39,10 @@ function App() {
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/release-view" element={<ReleaseView />} />
           <Route path="/blog-view" element={<BlogView />} />
-
           <Route path="/testimonials" element={<TestimonialsPage />} />
+
+          {/* ✅ Fallback for unknown paths */}
+          <Route path="*" element={<Home />} />
         </Routes>
         <Footer />
       </Router>
